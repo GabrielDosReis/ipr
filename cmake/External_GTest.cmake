bbd077b985d8f23a503204d45ebd25802acea47c
Gabor Horvath <xazax.hun@gmail.com>
2020-08-06 18:27:34 +0200

    Build the gtest package.

--- cmake/External_GTest.cmake
+++ cmake/External_GTest.cmake
@@ -4,24 +4,23 @@ include(ExternalProject)
 ExternalProject_Add(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG release-1.10.0
+  PREFIX "${CMAKE_BINARY_DIR}/gtest"
+  CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/gtest"
   UPDATE_COMMAND ""
-  INSTALL_COMMAND ""
   LOG_DOWNLOAD ON
   LOG_CONFIGURE ON
   LOG_BUILD ON)
 
-ExternalProject_Get_Property(googletest source_dir)
-set(GTEST_INCLUDE_DIRS ${source_dir}/googletest/include)
-set(GMOCK_INCLUDE_DIRS ${source_dir}/googlemock/include)
+set(GTEST_INSTALL ${CMAKE_BINARY_DIR}/gtest)
+set(GTEST_INCLUDE_DIRS ${GTEST_INSTALL}/include)
 
 # The cloning of the above repo doesn't happen until make, however if the dir doesn't
 # exist, INTERFACE_INCLUDE_DIRECTORIES will throw an error.
 # To make it work, we just create the directory now during config.
 file(MAKE_DIRECTORY ${GTEST_INCLUDE_DIRS})
-file(MAKE_DIRECTORY ${GMOCK_INCLUDE_DIRS})
 
-ExternalProject_Get_Property(googletest binary_dir)
-set(GTEST_LIBRARY_PATH ${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a)
+set(GTEST_LIBRARY_PATH ${GTEST_INSTALL}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a)
 set(GTEST_LIBRARY gtest)
 add_library(${GTEST_LIBRARY} UNKNOWN IMPORTED)
 set_target_properties(${GTEST_LIBRARY} PROPERTIES
@@ -30,7 +29,7 @@ set_target_properties(${GTEST_LIBRARY} PROPERTIES
     "INTERFACE_INCLUDE_DIRECTORIES" "${GTEST_INCLUDE_DIRS}")
 add_dependencies(${GTEST_LIBRARY} googletest)
 
-set(GTEST_MAIN_LIBRARY_PATH ${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main.a)
+set(GTEST_MAIN_LIBRARY_PATH ${GTEST_INSTALL}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main.a)
 set(GTEST_MAIN_LIBRARY gtest_main)
 add_library(${GTEST_MAIN_LIBRARY} UNKNOWN IMPORTED)
 set_target_properties(${GTEST_MAIN_LIBRARY} PROPERTIES
@@ -39,7 +38,7 @@ set_target_properties(${GTEST_MAIN_LIBRARY} PROPERTIES
     "INTERFACE_INCLUDE_DIRECTORIES" "${GTEST_INCLUDE_DIRS}")
 add_dependencies(${GTEST_MAIN_LIBRARY} googletest)
 
-set(GMOCK_LIBRARY_PATH ${binary_dir}/googlemock/${CMAKE_FIND_LIBRARY_PREFIXES}gmock.a)
+set(GMOCK_LIBRARY_PATH ${GTEST_INSTALL}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gmock.a)
 set(GMOCK_LIBRARY gmock)
 add_library(${GMOCK_LIBRARY} UNKNOWN IMPORTED)
 set_target_properties(${GMOCK_LIBRARY} PROPERTIES
@@ -48,7 +47,7 @@ set_target_properties(${GMOCK_LIBRARY} PROPERTIES
     "INTERFACE_INCLUDE_DIRECTORIES" "${GMOCK_INCLUDE_DIRS}")
 add_dependencies(${GMOCK_LIBRARY} googletest)
 
-set(GMOCK_MAIN_LIBRARY_PATH ${binary_dir}/googlemock/${CMAKE_FIND_LIBRARY_PREFIXES}gmock_main.a)
+set(GMOCK_MAIN_LIBRARY_PATH ${GTEST_INSTALL}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gmock_main.a)
 set(GMOCK_MAIN_LIBRARY gmock_main)
 add_library(${GMOCK_MAIN_LIBRARY} UNKNOWN IMPORTED)
 set_target_properties(${GMOCK_MAIN_LIBRARY} PROPERTIES
