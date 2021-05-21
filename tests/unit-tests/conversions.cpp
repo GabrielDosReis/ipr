4526a01e1b21ec156b18b24c496d0c021f7b3a49
Daniel Winsor <danwin@microsoft.com>
2021-05-21 14:45:04 -0700

    Adjusted tests to illustrate conversions correctly

--- tests/unit-tests/conversions.cpp
+++ tests/unit-tests/conversions.cpp
@@ -52,13 +52,6 @@ TEST_CASE("C++ Standard Conversions") {
   );
   lexicon.make_if(condition, *lexicon.make_expr_stmt(then_expr));
 
-  // Bitcast                      (Pretend)
-  INFO("(bool*)ptr");
-  lexicon.make_pretend(
-    *lexicon.make_id_expr(ptr),
-    lexicon.get_pointer(lexicon.bool_type()),
-    lexicon.get_pointer(lexicon.bool_type())
-  );
 }
 
 TEST_CASE("Class Conversions") {
@@ -73,35 +66,37 @@ TEST_CASE("Class Conversions") {
   auto& derived = *lexicon.make_class(*unit.global_region());
   derived.declare_base(base);
 
-  INFO("Base b;");
-  INFO("Derived d;");
+  INFO("Base* b;");
+  INFO("Derived* d;");
   auto& base_ptr = lexicon.get_pointer(base);
   auto& derived_ptr = lexicon.get_pointer(derived);
   auto& b = *unit.global_region()->declare_var(*lexicon.make_identifier("b"), base_ptr);
   auto& d = *unit.global_region()->declare_var(*lexicon.make_identifier("d"), derived_ptr);
 
   // Derived-to-base conversion
-  INFO("(Base*)b;");
-  lexicon.make_narrow(
-    *lexicon.make_id_expr(d),
-    base,
-    base_ptr
-  );
+  INFO("b = d;");
+  lexicon.make_assign(
+    *lexicon.make_id_expr(b),
+    *lexicon.make_widen(
+      *lexicon.make_id_expr(d),
+      base,
+      base_ptr
+  ));
 
   // Checked base-to-derived conversion
   INFO("dynamic_cast<Derived*>(b);");
-  lexicon.make_widen(
+  lexicon.make_narrow(
     *lexicon.make_id_expr(b),
     derived,
     derived_ptr
   );
 
-  // Unchecked base-to-derived conversion
+  // Unchecked base-to-derived conversion has no abstract
+  // representation in IPR so keep explicit cast.
   INFO("(Derived*)b;");
-  lexicon.make_pretend(
-    *lexicon.make_id_expr(b),
+  lexicon.make_cast(
     derived_ptr,
-    derived_ptr
+    *lexicon.make_id_expr(b)
   );
 }
 
@@ -112,11 +107,11 @@ TEST_CASE("CV Conversions") {
   impl::Interface_unit unit{lexicon, module};
 
   // Standard C++ CV qualification
-  INFO("(volatile int)7;");
+  INFO("(int) -> (volatile int)");
   lexicon.make_qualification(
-    *lexicon.make_literal(lexicon.double_type(), "2.2"),
+    *lexicon.make_literal(lexicon.int_type(), "7"),
     Type_qualifier::Volatile,
-    lexicon.int_type() // prvalue will be adjusted to remove qualifiers (see 7.2.2/2)
+    lexicon.int_type() // prvalue can be adjusted to remove qualifiers (see 7.2.2/2)
   );
 
   INFO("const int* ptr;");
@@ -124,15 +119,11 @@ TEST_CASE("CV Conversions") {
     Type_qualifier::Const, lexicon.get_pointer(lexicon.int_type()));
   auto& ptr = *unit.global_region()->declare_var(*lexicon.make_identifier("ptr"), ptr_type);
 
-  // Removal of const is non-standard C++ conversion
-  // Keep original explicit Const_cast
+  // Removal of const is a non-implicit conversion
   INFO("const_cast<int* const>(ptr);");
   lexicon.make_const_cast(
     lexicon.get_qualified(Type_qualifier::Const, lexicon.get_pointer(lexicon.int_type())),
     *lexicon.make_id_expr(ptr)
-    // TODO: The constraint to this expression should be different to the target type
-    // but the impl groups the two together. Should I change how casts are implemented??
-    // &lexicon.get_pointer(lexicon.int_type()) // prvalue will be adjusted to remove qualifiers (see 7.2.2/2)
   );
 
   INFO("int** ptr_ptr");
@@ -140,18 +131,34 @@ TEST_CASE("CV Conversions") {
   auto& ptr_ptr = *unit.global_region()->declare_var(
     *lexicon.make_identifier("ptr_ptr"), ptr_ptr_type);
 
-  INFO("(const int** const)ptr_ptr");
-  const auto& constraint = lexicon.get_pointer(lexicon.get_pointer(
-    lexicon.get_qualified(Type_qualifier::Const, lexicon.int_type())));
+  INFO("(int**) -> (int* const* const)");
   lexicon.make_qualification(
-    // TODO: how do you handle nesting as mentioned in #117. This feels wrong to
-    // only support qualification of the top level type.
-    *lexicon.make_coercion(
-      *lexicon.make_id_expr(ptr),
-      constraint,
-      constraint
+    *lexicon.make_qualification(
+      *lexicon.make_id_expr(ptr_ptr),
+      Type_qualifier::Const,
+      lexicon.get_qualified(Type_qualifier::Const, ptr_ptr.type())
     ),
     Type_qualifier::Const,
-    constraint // prvalue will be adjusted to remove qualifiers (see 7.2.2/2)
+    lexicon.get_pointer(lexicon.get_qualified(Type_qualifier::Const,
+      lexicon.get_pointer(lexicon.int_type())))
+      // prvalue can be adjusted to remove qualifier on top-level (see 7.2.2/2)
+  );
+
+  INFO("const int var = 0;");
+  auto& var = *unit.global_region()->declare_var(*lexicon.make_identifier("var"), 
+    lexicon.get_qualified(Type_qualifier::Const, lexicon.int_type()));
+
+  // Pretend can be used to explicitly reperesent automatic type adjustment as detailed
+  // in (7.2.2/2). Compilers are likely to just apply this adjustment on constraints without
+  // explicitly providing this node.
+  INFO("&var;");
+  // Automatic-adjustment         (Pretend)
+  lexicon.make_pretend(
+    *lexicon.make_address(
+      *lexicon.make_id_expr(var),
+      &lexicon.get_qualified(Type_qualifier::Const, lexicon.int_type())
+    ),
+    lexicon.int_type(),
+    lexicon.int_type()
   );
 }
