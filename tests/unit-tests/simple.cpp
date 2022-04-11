9060defa54ae975d72817bc18e6b205a9ecf71bd
Gabriel Dos Reis <gdr@axiomatics.org>
2022-04-11 10:09:34 -0700

    `name_factory`: factories for names
    The patch that moved the `Name` hierarchy out of the `Expr` hierarchy left the factory functions as member of `expr_factory`.  This patch sequesters them in a dedicated factory type (`name_factory`), and:
    
    - rename the `make_xxx` name factory functions to `get_xxx`
    
    - change their return type to reference to interface classes, instead of mutable pointer to implementation classes
    
    These changes represent source-level breaking changes, but the fixes are mechanical.

--- tests/unit-tests/simple.cpp
+++ tests/unit-tests/simple.cpp
@@ -12,9 +12,9 @@ TEST_CASE("global constant variable can be printed") {
 
   impl::Scope* global_scope = unit.global_scope();
 
-  const Name* name = lexicon.make_identifier("bufsz");
+  auto& name = lexicon.get_identifier("bufsz");
   auto& type = lexicon.get_qualified(Type_qualifiers::Const, lexicon.int_type());
-  impl::Var* var = global_scope->make_var(*name, type);
+  impl::Var* var = global_scope->make_var(name, type);
   var->init = lexicon.make_literal(lexicon.int_type(), "1024");
 
   std::stringstream ss;
@@ -32,9 +32,9 @@ TEST_CASE("Can create and print line numbers")
 
   impl::Scope* global_scope = unit.global_scope();
 
-  const Name* name = lexicon.make_identifier("bufsz");
+  auto& name = lexicon.get_identifier("bufsz");
   auto& type = lexicon.get_qualified(Type_qualifiers::Const, lexicon.int_type());
-  impl::Var* var = global_scope->make_var(*name, type);
+  impl::Var* var = global_scope->make_var(name, type);
   var->init = lexicon.make_literal(lexicon.int_type(), "1024");
 
   Source_location loc{ Line_number{1}, Column_number{2}, File_index{1} };
